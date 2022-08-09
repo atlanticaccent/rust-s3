dd285dd3dc1b128f1cae13a98cacdd78d01bb9c3
atlanticaccent <atlanticaccent@users.noreply.github.com>
2022-08-09 23:43:55 +0000

    deploy: a8c4ff368a646a954e3d553a9f6a245f9d108724

---
+++ implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["awscreds"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.143/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"awscreds/struct.Credentials.html\" title=\"struct awscreds::Credentials\">Credentials</a>","synthetic":false,"types":["awscreds::credentials::Credentials"]}];
+implementors["s3"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.143/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"s3/serde_types/struct.Part.html\" title=\"struct s3::serde_types::Part\">Part</a>","synthetic":false,"types":["s3::serde_types::Part"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
